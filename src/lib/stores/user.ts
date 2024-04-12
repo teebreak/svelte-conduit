53023fe1ea551e3ce0a0102d50177ebf817bd99b
Danylo Shamaiev <danyloshamaiev@gmail.com>
2024-04-12 22:08:08 +0300

    Rename the file to fix SPA mode

--- src/lib/stores/user.ts
+++ src/lib/stores/user.ts
@@ -1,15 +1,12 @@
 import { writable } from 'svelte/store';
 import type User from '$lib/models/User';
-import { browser } from '$app/environment';
 
 export const user = writable<User | null>(null);
 
 user.subscribe((value) => {
-	if (browser) {
-		if (value) {
-			localStorage.setItem('token', value.token);
-		} else {
-			localStorage.removeItem('token');
-		}
+	if (value) {
+		localStorage.setItem('token', value.token);
+	} else {
+		localStorage.removeItem('token');
 	}
 });
